d921ae87dd4ab8e8b0d55c217b73af64a6844b08
yourusername <tungtrungtk@gmail.com>
2022-06-09 21:17:13 +0900

    commit

--- index.js
+++ index.js
@@ -18,9 +18,8 @@ mongoose.connect(url,err=>{
     if(err) throw err;
     console.log("DB Connect")
 })
-if(process.env.NODE_ENV === 'production')
 app.get('*', (req, res) => {
-res.sendFile(path.join(__dirname,'todo', 'build', 'index.html'));
+res.sendFile(path.resolve(__dirname,'todo', 'build', 'index.html'));
 });
 app.get('/',(req,res)=>{
     res.json("Hello")
