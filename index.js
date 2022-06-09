c4b3387d29203cbe3126eddeb43b8554baa0aee8
yourusername <tungtrungtk@gmail.com>
2022-06-09 20:44:19 +0900

    finsh-project

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-
+const path = require('path')
 const app = express();
 
 app.use(express.json());
@@ -17,7 +17,10 @@ mongoose.connect(url,err=>{
     if(err) throw err;
     console.log("DB Connect")
 })
-
+app.use(express.static('client/build'));
+app.get('*', (req, res) => {
+res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 app.get('/',(req,res)=>{
     res.json("Hello")
 })
